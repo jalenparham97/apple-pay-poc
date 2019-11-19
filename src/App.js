35ba22e7602bb0401cd605342906a1bd8df2d3ae
jalenparham97 <jalenparham97@gmail.com>
2019-11-19 11:09:41 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const beginPayment = () => {
+    if (window.ApplePaySession) {
+      let paymentRequest = {
+        countryCode: 'US',
+        currencyCode: 'USD',
+        supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
+        merchantCapabilities: ['supports3DS'],
+        shippingMethods: [
+          {
+            "label": "Free Shipping",
+            "detail": "Arrives in 5 to 7 days",
+            "amount": "0.00",
+            "identifier": "FreeShip"
+          },
+          {
+            "label": "Express Shipping",
+            "detail": "Arrives in 1 to 2 days",
+            "amount": "5.00",
+            "identifier": "Express"
+          },
+        ],
+        shippingType: "shipping",
+        total: { label: 'Me my money', amount: '1.99'},
+        requiredShippingContactFields: ["name", "postalAddress" ],
+      }
+    
+      const session = new window.ApplePaySession(6, paymentRequest);
+      session.begin()    
+    }
+  }
+
+  const renderBtn = () => {
+    if (window.ApplePaySession && window.ApplePaySession.canMakePayments()) {
+        return <div onClick={() => beginPayment()} className="apple-pay-button apple-pay-button-black"></div>
+    } else {
+      return <div>not supported</div>
+    }
+  }
+  
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      {renderBtn()}
+    </div>
+  );
+}
+
+export default App;
